27c76a7cd2abcaa9288948bfeefe3ab6c7154b03
Your Name <you@example.com>
2017-03-27 13:25:04 +0200

    tenemos las 7 tareas

--- ceiling.c
+++ ceiling.c
@@ -18,6 +18,7 @@ static pthread_mutex_t mutexLuz;
 static pthread_mutex_t mutexPot;
 static pthread_mutex_t mutexTemp;
 static pthread_mutex_t mutexColision;
+static pthread_mutex_t mutexMotorDerecho;
 
 //consignas de sensores
 int luz_ADC; 	//lectura del sensor de luz
@@ -36,6 +37,8 @@ int peligro_pot;
 int peligro_temp;
 int peligro_luz;
 
+int v_motor_derecho;
+
 #define TRUE 1
 #define FALSE 0
 
@@ -51,6 +54,7 @@ int peligro_luz;
 #define PERIOD_POTEN	TIME_SCALER * 500
 #define PERIOD_RIESGO	TIME_SCALER * 1000
 #define PERIOD_COMM		TIME_SCALER * 3000
+#define PERIOD_NAVEGA	TIME_SCALER * 100
 
 #define BUTTON_PIN 3
 
@@ -108,6 +112,17 @@ void *thread_COMM(void *param)
   while (1) {
     wait_next_activation(p_d);
     t4_Comm();
+  }
+}
+
+void *thread_NAVEGA(void *param)
+{
+  struct periodic_task *p_d;
+
+  p_d = start_periodic_timer(0, PERIOD_NAVEGA);
+  while (1) {
+    wait_next_activation(p_d);
+    t7_Navegacion();
   }
 }
 
@@ -261,6 +276,18 @@ void t4_Comm(void)
 	printf("demora t4_Comm: %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
 #endif
 
+	pthread_mutex_lock(&mutexMotorDerecho);
+#if PRINT_TIME_MUTEX_SINGLE
+	struct timespec ts1, ts2;
+	clock_gettime( CLOCK_REALTIME, &ts1 );
+#endif
+	printf("\n¡v_motor_derecho! %d",v_motor_derecho);
+#if PRINT_TIME_MUTEX_SINGLE
+	clock_gettime( CLOCK_REALTIME, &ts2);
+	printf("demora t4_Comm mutexMotorDerecho: %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
+#endif
+	pthread_mutex_unlock(&mutexMotorDerecho);  
+
 }
 
 void t5_Riesgo(void)
@@ -371,6 +398,131 @@ void t6_colison ()
 #endif
 }
 
+void t7_Navegacion ()
+{
+
+	int local_peligro_pot;
+	int local_peligro_luz;
+	int local_peligro_temp;
+	int local_peligro_colision;
+	
+	int local_peligro;
+	int local_velocidad_motor_derecho;
+//---------------------
+#if PRINT_TIME
+	struct timespec ts1, ts2;
+	clock_gettime( CLOCK_REALTIME, &ts1 );
+#endif
+    
+	pthread_mutex_lock(&mutexTemp);
+#if PRINT_TIME_MUTEX_NAVEGACION
+	struct timespec ts1, ts2;
+	clock_gettime( CLOCK_REALTIME, &ts1 );
+#endif
+
+	local_peligro_temp=peligro_temp;
+	
+#if PRINT_TIME_MUTEX_NAVEGACION
+	clock_gettime( CLOCK_REALTIME, &ts2);
+	printf("demora t7_Navegacion mutexTemp: %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
+#endif
+	pthread_mutex_unlock(&mutexTemp);  
+//---------------------
+	pthread_mutex_lock(&mutexPot);
+#if PRINT_TIME_MUTEX_NAVEGACION
+	//struct timespec ts1, ts2;
+	clock_gettime( CLOCK_REALTIME, &ts1 );
+#endif
+
+	local_peligro_pot=peligro_pot;
+	
+#if PRINT_TIME_MUTEX_NAVEGACION
+	clock_gettime( CLOCK_REALTIME, &ts2);
+	printf("demora t7_Navegacion mutexPot: %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
+#endif
+	pthread_mutex_unlock(&mutexPot); 
+//---------------------
+	pthread_mutex_lock(&mutexLuz);
+
+#if PRINT_TIME_MUTEX_NAVEGACION
+	//struct timespec ts1, ts2;
+	clock_gettime( CLOCK_REALTIME, &ts1 );
+#endif
+
+	local_peligro_luz=peligro_luz;
+	
+#if PRINT_TIME_MUTEX_TRIESGO
+	clock_gettime( CLOCK_REALTIME, &ts2);
+	printf("demora t7_Navegacion mutexLuz: %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
+#endif
+	pthread_mutex_unlock(&mutexLuz); 
+
+//--------------------- 
+	pthread_mutex_lock(&mutexColision);
+#if PRINT_TIME_MUTEX_SINGLE
+	struct timespec ts1, ts2;
+	clock_gettime( CLOCK_REALTIME, &ts1 );
+#endif
+	local_peligro_colision=colision;
+#if PRINT_TIME_MUTEX_SINGLE
+	clock_gettime( CLOCK_REALTIME, &ts2);
+	printf("demora t7_Navegacion mutexColision: %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
+#endif
+	pthread_mutex_unlock(&mutexColision);  
+//--------------------- 
+
+
+	//printf("\n local_peligro_colision! %d",local_peligro_colision);
+	//printf("\n local_peligro_luz! %d",local_peligro_luz);
+	//printf("\n local_peligro_pot! %d",local_peligro_pot);
+	//printf("\n local_peligro_temp! %d",local_peligro_temp);
+	//printf("\n local_peligro! %d",local_peligro);
+
+	local_peligro=local_peligro_temp + local_peligro_pot + local_peligro_luz;
+	
+	if(local_peligro_colision)
+	{
+		local_velocidad_motor_derecho=0;
+	}
+	else
+	{
+		switch (local_peligro)
+		{
+			case 0:
+				local_velocidad_motor_derecho=100;
+				break;
+			case 1:
+				local_velocidad_motor_derecho=70;
+				break;
+			case 2:
+				local_velocidad_motor_derecho=40;
+				break;
+			case 3:
+				local_velocidad_motor_derecho=10;
+				break;
+		}
+	}
+
+
+	pthread_mutex_lock(&mutexMotorDerecho);
+#if PRINT_TIME_MUTEX_SINGLE
+	struct timespec ts1, ts2;
+	clock_gettime( CLOCK_REALTIME, &ts1 );
+#endif
+	v_motor_derecho=local_velocidad_motor_derecho;
+	//printf("\n¡Colision! %d",colision);
+#if PRINT_TIME_MUTEX_SINGLE
+	clock_gettime( CLOCK_REALTIME, &ts2);
+	printf("demora t7_Navegacion mutexMotorDerecho: %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
+#endif
+	pthread_mutex_unlock(&mutexMotorDerecho);  
+
+#if PRINT_TIME
+	clock_gettime( CLOCK_REALTIME, &ts2);
+	printf("demora t7_Navegacion : %f\n", (float) (1.0 * (1.0 * ts2.tv_nsec - ts1.tv_nsec * 1.0) * 1e-9 + 1.0 * ts2.tv_sec - 1.0 * ts1.tv_sec ));
+#endif
+}
+
 
 void miTareaEsporadica (void)
 {
@@ -425,9 +577,10 @@ int main(int argc, char *argv[])
 {
   int err;
   void *returnvalue;
-  pthread_t thread_LUZ_id, thread_POT_id, thread_TEMP_id, thread_RIESGO_id, thread_COMM_id;
+  pthread_t thread_LUZ_id, thread_POT_id, thread_TEMP_id, thread_RIESGO_id, thread_COMM_id, thread_NAVEGA_id;
   int prioridad_min;
   int p_luz=0;
+  int p_navega=0;
   int p_comm=0;
   int p_coli=0;
   int p_temp=3;
@@ -471,6 +624,7 @@ int main(int argc, char *argv[])
   p_pot=prioridad_min;
   p_riesgo+=prioridad_min;
   p_comm+=prioridad_min;
+  p_navega+=prioridad_min;
 
   //-------- creation of mutex 
 
@@ -506,9 +660,25 @@ int main(int argc, char *argv[])
   if (pthread_mutex_init (&mutexColision,&mymutexattr) != 0) {
     perror ("Error in mutex init");
   }
+  //-------- init mutex mutexMotorDerecho
+  if (pthread_mutex_init (&mutexMotorDerecho,&mymutexattr) != 0) {
+    perror ("Error in mutex init");
+  }
 
   pthread_mutexattr_destroy(&mymutexattr);
 
+  
+  
+  //--------  creation and activation of the new thread 
+  sp.sched_priority =  p_navega;
+  err = pthread_attr_setschedparam(&attrs, &sp);
+  if (err != 0) {
+    perror("setschedparam");
+  }
+  err = pthread_create(&thread_NAVEGA_id, &attrs, thread_NAVEGA, (void *)NULL);
+  if (err != 0) {
+    fprintf(stderr, "Cannot create thread_NAVEGA");
+  }  
   
   
   //--------  creation and activation of the new thread 
